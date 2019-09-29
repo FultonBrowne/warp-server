9e9df76d8ee7d94b437b2a66e66db0ffc4b90ab2
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-09-28 19:29:10 -0700

    playing with node

--- warp-server/server.js
+++ warp-server/server.js
@@ -1,8 +1,15 @@
 'use strict';
+//HTTP config
 var http = require('http');
+//Port
 var port = process.env.PORT || 1337;
+//url value
+var url;
+
 
 http.createServer(function (req, res) {
+    //content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    //parse input
+    if (res.url.startsWith(h="hi"))res.end(req.url);
 }).listen(port);
