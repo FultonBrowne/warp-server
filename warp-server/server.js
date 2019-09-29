42db5e94a20b4af4165886a766840217feb218a6
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-09-29 09:22:39 -0700

    fixed splash screen bug

--- warp-server/server.js
+++ warp-server/server.js
@@ -5,11 +5,19 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 //url value
 var url;
+let string = "hi"
+let bool = true
+let int = 0
 
 
 http.createServer(function (req, res) {
     //content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     //parse input
-    if (res.url.startsWith(h="hi"))res.end(req.url);
+    res.end(string);
+    //fun loop
+    while (bool) {
+        res.end(string);
+        
+    }
 }).listen(port);
